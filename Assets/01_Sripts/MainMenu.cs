4f0e573ecce6d60b1b7b9e70cd2692e15fde801a
RJoel <svr0035567@est.univalle.edu>
2026-08-19 22:50:14 -0400

    Update MainMenu functionality and scene management; add gameplay scene name and improve options handling

--- Assets/01_Sripts/MainMenu.cs
+++ Assets/01_Sripts/MainMenu.cs
@@ -5,24 +5,61 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject optionsMenu;
     public GameObject mainMenu;
+    [SerializeField] private string gameplaySceneName = "CameraTest";
 
-    //public void OpenOptionsPanel()
-    //{
-    //    mainMenu.SetActive(false);
-    //    optionsMenu.SetActive(true);
-    //}
-    //public void OpenMainMenuPanel()
-    //{
-    //    mainMenu.SetActive(true);
-    //    optionsMenu.SetActive(false);
-    //}
+    public void OpenOptionsPanel()
+    {
+        if (mainMenu != null)
+        {
+            mainMenu.SetActive(false);
+        }
+
+        if (optionsMenu != null)
+        {
+            optionsMenu.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("MainMenu: optionsMenu no esta asignado en el Inspector.");
+        }
+    }
+
+    public void OpenMainMenuPanel()
+    {
+        if (mainMenu != null)
+        {
+            mainMenu.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("MainMenu: mainMenu no esta asignado en el Inspector.");
+        }
+
+        if (optionsMenu != null)
+        {
+            optionsMenu.SetActive(false);
+        }
+    }
+
     public void QuitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit(); 
+#endif
     }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        if (Application.CanStreamedLevelBeLoaded(gameplaySceneName))
+        {
+            SceneManager.LoadScene(gameplaySceneName);
+        }
+        else
+        {
+            Debug.LogError("MainMenu: la escena '" + gameplaySceneName + "' no esta en Build Settings.");
+        }
     }
 
 
